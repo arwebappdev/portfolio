86c8d9370a3519f9d1c493240d65370e35d960e7
ar-mansoori <ar.appsoftwares@gmail.com>
2025-02-11 22:19:03 +0530

    Changed Lines on about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -70,7 +70,7 @@ const About = () => {
 
       <div className="relative left-0 top-0 translate-y-[15%] w-[70%] sm:w-1/2 h-fit ring-2 ring-gray-300 dark:ring-gray-700 text-4xl sm:text-5xl font-mono p-5 text-black dark:text-white">
         <DecryptedText
-          text="Hi, I am Abdul Rehman a curious FRONTEND Developer, who loves to create websites with Cool Animations and Creative UIs..."
+          text="Hi, I'm Abdul Rehman, a passionate Frontend Developer with a keen eye for design and a love for crafting interactive, visually stunning websites. I specialize in building creative UIs with smooth animations, ensuring an engaging user experience."
           animateOn="view"
           revealDirection="center"
           speed={50}
